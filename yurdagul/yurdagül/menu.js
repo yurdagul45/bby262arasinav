abea5913391a9b85a5d4fd6297544a51ee9952a6
yurdagul45 <37695345+yurdagul45@users.noreply.github.com>
2018-04-13 12:08:40 +0300

    Add files via upload

---
+++ yurdagul/yurdagül/menu.js
@@ -0,0 +1,20 @@
+var mymenu=
+   '<div class="collapse navbar-collapse" id="navbarsExampleDefault">'+
+   '<ul class="navbar-nav mr-auto">'+
+    '<li class="nav-item active"><a class="nav-link" href="index.html">Ana Sayfa <span class="sr-only">(current)</span></a></li>'+
+    '<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="http://example.com/" id="dropdown01" data-toggle="dropdown" aria-    haspopup="true" aria-expanded="false">Kütüphanemiz</a>'+
+    '<div class="dropdown-menu" aria-labelledby="dropdown01">'+
+    '<a class="dropdown-item" href="yonetim.html">Yönetim</a>'+
+    '<a class="dropdown-item" href="tarihce.html">Tarihçesi</a>'+
+    '<a class="dropdown-item" href="hakkimizda.html">Hakkımızda</a>'+
+    '</div>'+
+    '</li>'+            
+    '<li class="nav-item"><a class="nav-link" href="duyurular.html">Duyurular</a></li>'+
+    '<li class="nav-item"><a class="nav-link" href="etkinlikler.html">Etkinlikler</a></li> '+
+    '<li class="nav-item"><a class="nav-link" href="iletisim.html">İletişim</a></li>'+
+    '</ul>'+
+    '<form class="form-inline my-2 my-lg-0">'+
+    '<button class="btn btn-outline-success my-2 my-sm-0" type="submit">↳ Giriş</button>'+
+    '</form></div>';
+
+var copy='<p>© Copyright 2018</p>';
